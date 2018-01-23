2e18d315899376254df02a34ad69b390eb65c24c
Victor Jordan <victorjordan95@gmail.com>
2018-01-23 21:40:05 -0200

    removendo favico

--- web.js
+++ web.js
@@ -1,4 +1,6 @@
-var express = require(‘express’);
+var express = require('express');
 var app = express();
-app.use(express.static(__dirname + ‘/app’)); //aqui você define onde está o index.html da sua aplicação.
-app.listen(process.env.PORT || 3000);
+app.use(express.static(__dirname + '/app'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
